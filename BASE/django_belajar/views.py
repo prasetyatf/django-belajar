efa63a065e5bfb14f613e49aec7e308aba5ae90b
prasetyatf <tegarfitriaji@gmail.com>
2023-06-07 17:27:36 +0700

    belajar dtl, dan static file

--- BASE/django_belajar/views.py
+++ BASE/django_belajar/views.py
@@ -4,6 +4,17 @@ from django.shortcuts import render
 def index(request):
     context = {
         'judul':'Home',
-        'created_by':'Tegar'
+        'created_by':'Tegar',
+        'header':'<h1>Ini header</h1>',
+        'footer':'<h1>Ini footer</h1>',
+        'nav': [
+            ['/', 'Home'],
+            ['/blog', 'Blog'],
+            ['/sitemap', 'Sitemap'],
+        ],
+        'about':[
+            ['Nama:', 'Tegar'],
+            ['Hobi:', 'Ngoding']
+        ]
     }
     return render(request, 'index.html', context)
